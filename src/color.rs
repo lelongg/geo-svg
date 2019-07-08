3aa3939728b076b6d46e7fac56c85603e0b9d492
Gérald Lelong <gerald.lelong@easymov.fr>
2019-07-08 17:10:55 +0200

    add hsl colors

--- src/color.rs
+++ src/color.rs
@@ -5,6 +5,7 @@ pub enum Color {
     Named(&'static str),
     Rgb(u8, u8, u8),
     Hex(u32),
+    Hsl(u16, u8, u8),
 }
 
 impl Display for Color {
@@ -13,6 +14,9 @@ impl Display for Color {
             Color::Named(name) => write!(fmt, "{}", name),
             Color::Rgb(r, g, b) => write!(fmt, "rgb({},{},{})", r, g, b),
             Color::Hex(hex) => write!(fmt, "{:#X}", hex),
+            Color::Hsl(h, s, l) => {
+                write!(fmt, "hsl({},{}%,{}%)", h % 360, s.min(&100), l.min(&100))
+            }
         }
     }
 }
